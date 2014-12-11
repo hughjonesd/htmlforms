a8ec5af27df5b34bc4572d16e3ff7f414f89cf68
David Hugh-Jones <davidhughjones@gmail.com>
2014-12-11 00:09:32 +0000

    tests

--- inst/tests/tests.R
+++ inst/tests/tests.R
@@ -1,11 +1,63 @@
 
 
+library(testthat)
 
-f1 <- form(action="http://foo.com", method="POST", type="x-www-form")
-ti <- text_input(name='name', default='Pete')
-si <- select_input(name='agegroup', options=c(a="< 18", b="over 18"))
+context("Checks")
 
-add_element(f1, ti)
-add_element(f1, si)
-print(f1)
-cat(f1)
+test_that("checker works", {
+  check13 <- checker(x >= 1 && x <= 3, "error")
+  expect_true(
+    check13(3, list())
+  )
+  expect_match(
+    check13(4, list())
+  , "error")
+  
+  checkval <- checker(x == vals$y, "error")
+  expect_true(
+    checkval(3, list(y=3))
+  )
+  expect_match(
+    checkval(4, list(y=3))
+  , "error")
+})
+
+test_that("checker works in context", {
+  y <- 4
+  check_y <- checker(x==y, "myerror")
+  expect_true(
+    check_y(4, list())
+  )
+  expect_match(
+    check_y(3, list())
+  , "myerror")
+})
+
+test_that("check_length works", {
+  chl <- check_length(2,4)
+  expect_true(
+    chl("foo", list())
+  )
+  expect_match(
+    chl("foobar", list())
+  , "4")
+})
+
+test_that("R errors dealt with right", {
+  check_fail <- checker(nonexistent, "myerror")
+  expect_that(
+    check_fail(1, list())
+  , throws_error())
+# expect_that bug:
+#   check_fail <- checker(nonexistent, "myerror", error=warning)
+#   expect_that(
+#     check_fail(1, list())
+#   , gives_warning())
+#   expect_match(
+#     check_fail(1, list())
+#   , "myerror")
+  check_fail <- checker(nonexistent, "myerror", error=identity)
+  expect_match(
+    check_fail(1, list())
+  , "myerror")
+})
